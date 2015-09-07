3a6a845380deb6bed766318bf2a59779d8819487
Marcelo Lomeña <marcelol@loso.cl>
2015-09-07 16:03:03 -0300

    mm

--- BDC/project/Build.scala
+++ BDC/project/Build.scala
@@ -1,6 +1,5 @@
 import sbt._
 import Keys._
-//import play.Project._
 import play.api._
 import play.PlayImport._
 
@@ -16,17 +15,12 @@ object ApplicationBuild extends Build {
     anorm,
     "net.liftweb" %% "lift-json" % "2.6+",
     "net.sourceforge.jtds" % "jtds" % "1.2",
-    //"mysql" % "mysql-connector-java" % "5.1.18",
     "org.json" % "org.json" % "chargebee-1.0",
     "org.mindrot" % "jbcrypt" % "0.3m",
-    //"org.mongodb" %% "casbah" % "2.7.2",
-    //"org.json4s" %% "json4s-jackson" % "3.2.10",
-    //"org.json4s" %% "json4s-mongo" % "3.2.10",
     "org.imgscalr" % "imgscalr-lib" % "4.2",
     "org.apache.poi" % "poi" % "3.9",
     "org.apache.poi" % "poi-ooxml" % "3.9",
     "net.sourceforge" % "mpxj" % "4.5.0-rc1",
-    //"com.google.code.gson" % "gson" % "2.2.4",
     "com.typesafe.play" %% "play-mailer" % "2.4.1",
     "com.typesafe" %% "play-plugins-mailer" % "2.1.0",
     "org.apache.commons" % "commons-email" % "1.3")
