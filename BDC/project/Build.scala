114cdaa5b972034e54e18a694691cd38bb15e726
Marcelo Lomeña <marcelol@loso.cl>
2015-09-14 23:02:34 -0300

    mm

--- BDC/project/Build.scala
+++ BDC/project/Build.scala
@@ -7,20 +7,18 @@ object ApplicationBuild extends Build {
 
   val appName = "BDC"
   val appVersion = "1.0-SNAPSHOT"
-//val lift_json = "net.liftweb" %% "lift-json" % "XXX"
+
   val appDependencies = Seq(
     // Add your project dependencies here,
     ws,
     jdbc,
     anorm,
     "net.liftweb" %% "lift-json" % "2.6+",
-    "net.sourceforge.jtds" % "jtds" % "1.2",
     "org.json" % "org.json" % "chargebee-1.0",
     "org.mindrot" % "jbcrypt" % "0.3m",
     "org.imgscalr" % "imgscalr-lib" % "4.2",
     "org.apache.poi" % "poi" % "3.9",
     "org.apache.poi" % "poi-ooxml" % "3.9",
-    //"net.sourceforge" % "mpxj" % "4.5.0-rc1",
     "com.typesafe.play" %% "play-mailer" % "2.4.1",
     "com.typesafe" %% "play-plugins-mailer" % "2.1.0",
     "org.apache.commons" % "commons-email" % "1.3")
