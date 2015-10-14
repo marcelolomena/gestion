5598e3ed96c5cca0bf55ab722eeaf982a9e31dcb
Marcelo Lomeña <marcelol@loso.cl>
2015-10-14 14:58:28 -0300

    mm

--- BDC/app/controllers/Frontend/Incident.scala
+++ BDC/app/controllers/Frontend/Incident.scala
@@ -2,16 +2,120 @@ package controllers.Frontend
 
 import play.api.mvc.Action
 import play.api.mvc.Controller
+import services.IncidentService
+import org.apache.commons.lang3.StringUtils
+import org.json.JSONArray
+import org.json.JSONObject
+import net.liftweb.json._
+import net.liftweb.json.JsonParser._
+import models.DBFilter
+import utils.FormattedOutPuts
 
 /**
  * @author marcelo
  */
 object Incident extends Controller {
-  def list = Action {
+
+  def fromIncidentName(choice: String, value: String): String = choice match {
+    case "brief_description" => " '%" + value + "%' "
+    case _                   => "error"
+  }
+
+  def home = Action {
     implicit request =>
       request.session.get("username").map { user =>
 
         Ok(views.html.frontend.incident.listIncident()).withSession("username" -> request.session.get("username").get, "utype" -> request.session.get("utype").get, "uId" -> request.session.get("uId").get, "user_profile" -> request.session.get("user_profile").get)
+      }.getOrElse {
+        Redirect(routes.Login.loginUser()).withNewSession
+      }
+
+  }
+
+  def list = Action {
+    implicit request =>
+      request.session.get("username").map { user =>
+
+        val rows = request.getQueryString("rows").getOrElse("20").toString()
+        val page = request.getQueryString("page").getOrElse("1").toString()
+        val filters = request.getQueryString("filters").getOrElse("").toString()
+
+        var panel: Seq[models.Incident] = null
+        var records: Int = 0
+
+        var node = new JSONObject()
+        var tieneJson = true
+        var qrystr = ""
+
+        if (!StringUtils.isEmpty(filters)) {
+
+          val jObject: play.api.libs.json.JsValue = play.api.libs.json.Json.parse(filters)
+
+          if (!jObject.\\("rules").isEmpty) {
+
+            val jList = jObject.\\("rules")
+            var jElement = ""
+            for (j <- jList) {
+              jElement = j.toString()
+              if (jElement.equals("[]")) {
+                tieneJson = false
+              } else {
+
+                implicit val formats = DefaultFormats
+                val json = net.liftweb.json.JsonParser.parse(filters)
+                val elements = (json \\ "rules").children
+                for (acct <- elements) {
+                  val m = acct.extract[DBFilter]
+                  qrystr += m.field + FormattedOutPuts.fromPredicate(m.op) + fromIncidentName(m.field, m.data) + " AND "
+                }
+
+              }
+            }
+
+            if (tieneJson) {
+
+              records = IncidentService.count(qrystr)
+              panel = IncidentService.list(rows, page, qrystr)
+
+            } else {
+              records = IncidentService.count("")
+              panel = IncidentService.list(rows, page, "")
+
+            }
+          }
+        } else {
+          records = IncidentService.count("")
+          panel = IncidentService.list(rows, page, "")
+
+        }
+
+        var registro = new JSONArray()
+        for (p <- panel) {
+          var campo = new JSONObject()
+          campo.put("incident_id", p.incident_id)
+          campo.put("configuration_id", p.configuration_id)
+          campo.put("program_id", p.program_id)
+          campo.put("date_creation", p.date_creation)
+          campo.put("ir_number", p.ir_number)
+          campo.put("user_sponsor_id", p.user_sponsor_id)
+          campo.put("brief_description", p.brief_description)
+          campo.put("extended_description", p.extended_description)
+          campo.put("severity_id", p.severity_id)
+          campo.put("date_end", p.date_end)
+          campo.put("date_end", p.date_end)
+          campo.put("task_owner_id", p.task_owner_id)
+          campo.put("user_creation_id", p.user_creation_id)
+          campo.put("task_id", p.task_id)
+          registro.put(campo)
+        }
+        var pagedisplay = Math.ceil(records.toInt / Integer.parseInt(rows.toString()).toFloat).toInt
+
+        node.put("page", page)
+        node.put("total", pagedisplay)
+        node.put("records", records)
+        node.put("rows", registro)
+
+        Ok(node.toString()).withSession("username" -> request.session.get("username").get, "utype" -> request.session.get("utype").get, "uId" -> request.session.get("uId").get, "user_profile" -> request.session.get("user_profile").get)
       }.getOrElse {
         Redirect(routes.Login.loginUser()).withNewSession
       }
