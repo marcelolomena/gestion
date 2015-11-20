d5205d1f85ddf2547e22d0b038597c0a26e21ed6
Marcelo Lomeña <marcelol@loso.cl>
2015-11-20 16:09:36 -0300

    mmm

--- BDC/public/frontend/js/programa.js
+++ BDC/public/frontend/js/programa.js
@@ -19,55 +19,100 @@ $(document).ready(function(){
 
 	$.datepicker.setDefaults($.datepicker.regional['es']);	
 	
+	var programaModel = [
+	                    { label: 'program_id', name: 'program_id', width: 100, key: true, hidden:true }, 
+	                    { label: 'Programa', name: 'program_name', width: 300,formatter: returnProgramLink, search:false },
+	                    { label: 'División', name: 'division', width: 250,editable: false, hidden: false, editrules: {edithidden: true},
+	  	            	  stype: 'select',searchoptions: {dataUrl: '/listaDivisiones',
+	  	            		buildSelect: function (response) {
+	  	            			var data = JSON.parse(response);
+	  	            		    var s = "<select>";
+	  	            		    s += '<option value="0">--Sin División--</option>';
+	  	            		    $.each(data, function(i, item) {
+	  	            		    	s += '<option value="' + data[i].dId + '">' + data[i].division + '</option>';
+	  	            		    });
+	  	            		    return s + "</select>";
+	  	            		}
+	  	            		  } },
+	                    { label: 'Tipo de Programa', name: 'program_type', width: 150,editable: false, hidden: false, editrules: {edithidden: true},
+	  	  	            	  stype: 'select',searchoptions: {dataUrl: '/listaTipo',
+	  	  	            		buildSelect: function (response) {
+	  	  	            			var data = JSON.parse(response);
+	  	  	            		    var s = "<select>";
+	  	  	            		    s += '<option value="0">--Sin Tipo--</option>';
+	  	  	            		    $.each(data, function(i, item) {
+	  	  	            		    	s += '<option value="' + data[i].id + '">' + data[i].program_type + '</option>';
+	  	  	            		    });
+	  	  	            		    return s + "</select>";
+	  	  	            		}
+	  	  	            		  } },
+	                    { label: 'Foco Estratégico', name: 'sub_type', width: 150,editable: false, hidden: false, editrules: {edithidden: true},
+	  		  	  	            	  stype: 'select',searchoptions: {dataUrl: '/listaFoco',
+	  		  	  	            		buildSelect: function (response) {
+	  		  	  	            			var data = JSON.parse(response);
+	  		  	  	            		    var s = "<select>";
+	  		  	  	            		    s += '<option value="0">--Sin Foco--</option>';
+	  		  	  	            		    $.each(data, function(i, item) {
+	  		  	  	            		    	s += '<option value="' + data[i].id + '">' + data[i].sub_type + '</option>';
+	  		  	  	            		    });
+	  		  	  	            		    return s + "</select>";
+	  		  	  	            		}
+	  		  	  	            } },
+	                    { label: 'Estado', name: 'workflow_status', width: 150,editable: false, hidden: false, editrules: {edithidden: true},
+		  	  	            	  stype: 'select',searchoptions: {dataUrl: '/listaEstado',
+		  	  	            		buildSelect: function (response) {
+		  	  	            			var data = JSON.parse(response);
+		  	  	            		    var s = "<select>";
+		  	  	            		    s += '<option value="0">--Sin Estado--</option>';
+		  	  	            		    $.each(data, function(i, item) {
+		  	  	            		    	s += '<option value="' + data[i].id + '">' + data[i].workflow_status + '</option>';
+		  	  	            		    });
+		  	  	            		    return s + "</select>";
+		  	  	            		}
+		  	  	            } },
+	                    { label: 'Fecha Entrega',
+	                      name: 'release_date',
+	                      width: 100,
+	                      formatter: 'date',
+	                      formatoptions: { srcformat: 'Y-m-d', newformat: 'Y-m-d' },
+	                      searchoptions:{
+	        	              dataInit:function(el){
+	        		              	$(el).datepicker({
+	        			              	dateFormat:'yy-mm-dd',
+	        			              	changeYear: true,
+	        	                        changeMonth: true,                            
+	        	                        onSelect: function (dateText, inst) {
+	        	                            setTimeout(function () {
+	        	                                $('#jqGridProgram')[0].triggerToolbar();
+	        	                            }, 100);
+	        	                        }
+	        				        });
+	        		              },sopt: ["gt","lt","eq"]
+	                     }
+	                    },
+	                ];
+	
 	$("#jqGridProgram").jqGrid({
-        url: '/reportStateSubTask',
+        url: '/listadoPrograma',
         mtype: "GET",
         datatype: "json",
         page: 1,
-        colModel: [
-            { label: 'program_id', name: 'program_id', width: 100, key: true, hidden:true }, 
-            { label: 'División', name: 'division', width: 250 },
-            { label: 'Tipo Programa', name: 'tipo', width: 250 },
-            { label: 'Programa', name: 'programa', width: 250 },
-            { label: 'Fecha Entrega',
-              name: 'pfecini',
-              width: 100,
-              formatter: 'date',
-              formatoptions: { srcformat: 'Y-m-d', newformat: 'Y-m-d' },
-              searchoptions:{
-	              dataInit:function(el){
-		              	$(el).datepicker({
-			              	dateFormat:'yy-mm-dd',
-			              	changeYear: true,
-	                        changeMonth: true,                            
-	                        onSelect: function (dateText, inst) {
-	                            setTimeout(function () {
-	                                $('#jqGrid3')[0].triggerToolbar();
-	                            }, 100);
-	                        }
-				        });
-		              },sopt: ["gt","lt","eq"]
-             }
-            },
-        ],
+        colModel: programaModel,
         rowNum: 20,
         regional : 'es',
         height: 'auto',
-        autowidth:true,        
+        autowidth:true, 
+        shrinkToFit: false,
+        forceFit:true,
+        caption:'Lista de Programas',
         pager: "#jqGridProgramPager",
-        loadComplete: findWithColor
+        loadComplete: findWithColor,
+        editurl:"/programaSave",
+        viewrecords: true,
+        rowList: [5, 10, 20, 50],
+        gridview: true,
     });	
 	$("#jqGridProgram").jqGrid('filterToolbar', {stringResult: true,searchOperators: true, searchOnEnter: false, defaultSearch: 'cn'});
 	$("#jqGridProgram").jqGrid('navGrid','#jqGridProgramPager',{add:false,edit:false,del:false,search: false});
-	$("#jqGridProgram").jqGrid('navButtonAdd','#jqGridProgram',{
-	       caption:"",
-	       buttonicon : "silk-icon-page-excel",
-	       title: "Exportar a Excel", 
-	       onClickButton : function () { 
-	    	   var grid = $("#jqGridProgram");
-	           var rowKey = grid.getGridParam("selrow");
-	           var url = 'status-subtask';
-	    	   $("#jqGridProgram").jqGrid('excelExport',{"url":url});
-	       } 
-	});	
+
 });
