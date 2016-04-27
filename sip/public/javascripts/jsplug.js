2ac7ac36ffe7945dd26df744203ef4f691bc05d7
Marcelo Lomeña <marcelol@loso.cl>
2016-04-27 17:31:37 -0300

    mante iniciativa

--- sip/public/javascripts/jsplug.js
+++ sip/public/javascripts/jsplug.js
@@ -1,21 +1,36 @@
-$(document).ready(function() {
-  $("[data-toggle]").click(function() {
+/*
+$(document).ready(function () {
+  $("[data-toggle]").click(function () {
     var toggle_el = $(this).data("toggle");
     $(toggle_el).toggleClass("open-sidebar");
   });
-     
+
+});
+*/
+
+
+$(document).ready(function () {
+  $("[data-toggle]").click(function () {
+    var toggle_el = $(this).data("toggle");
+    $(toggle_el).toggleClass("open-sidebar");
+  });
 });
 
-$(".swipe-area").swipe({
-    swipeStatus:function(event, phase, direction, distance, duration, fingers)
-        {
-            if (phase=="move" && direction =="right") {
-                 $(".container").addClass("open-sidebar");
-                 return false;
-            }
-            if (phase=="move" && direction =="left") {
-                 $(".container").removeClass("open-sidebar");
-                 return false;
-            }
-        }
-});
+
+
+
+
+(function ($) {
+  $(".swipe-area").swipe({
+    swipeStatus: function (event, phase, direction, distance, duration, fingers) {
+      if (phase == "move" && direction == "right") {
+        $(".container").addClass("open-sidebar");
+        return false;
+      }
+      if (phase == "move" && direction == "left") {
+        $(".container").removeClass("open-sidebar");
+        return false;
+      }
+    }
+  });
+})(jQuery);
