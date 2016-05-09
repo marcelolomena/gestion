a9805c094a19c6929faa52e70b6ae54194f9fe22
Marcelo Lomeña <marcelol@loso.cl>
2016-05-09 18:54:16 -0300

    cagazo

---
+++ sip/models/erogacionproyecto.js
@@ -0,0 +1,113 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('erogacionproyecto', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    iddetalleproyecto: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'detalleproyecto',
+        key: 'id'
+      }
+    },
+    sap: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    idproveedor: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    rutproveedor: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    razonsocial: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    factura: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    fechafactura: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    fechacontabilizacion: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    contrato: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    anexo: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    codigoart: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    partida: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    idcuenta: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    cuentacontable: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    idcui: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    cui: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    idservicio: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    tarea: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    idmoneda: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    moneda: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    periodo: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    montoporperiodo: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    montopesos: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    borrado: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    schema: 'sip',timestamps: false,tableName: 'erogacionproyecto'
+  });
+};
