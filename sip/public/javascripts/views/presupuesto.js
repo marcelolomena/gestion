6d2f528e1e8c10fc2d7860091f8bf412ce0e2af9
Marcelo Lomeña <marcelol@loso.cl>
2016-05-24 11:43:47 -0300

    contratos

--- sip/public/javascripts/views/presupuesto.js
+++ sip/public/javascripts/views/presupuesto.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var tmpl = "<div id='responsive-form' class='clearfix'>";
 
     tmpl += "<div class='form-row'>";
-    tmpl += "<div class='column-full'>CUI {CUI}</div>";
+    tmpl += "<div class='column-full'>CUIiii {idcui}</div>";
     tmpl += "</div>";
 
     tmpl += "<div class='form-row'>";
@@ -16,18 +16,23 @@ $(document).ready(function () {
     tmpl += "<div class='form-row'>";
     tmpl += "<div class='column-full'>Descripción {descripcion}</div>";
     tmpl += "</div>";
-            
+
+    tmpl += "<div class='form-row' style='display: none;'>";
+    tmpl += "<div class='column-half'>CUI {CUI}</div>";
+    tmpl += "</div>";
+
     tmpl += "<hr style='width:100%;'/>";
     tmpl += "<div> {sData} {cData}  </div>";
-    tmpl += "</div>"; 
-    
+    tmpl += "</div>";
+
     var modelProyecto = [
         { label: 'id', name: 'id', key: true, hidden: true },
         {
             label: 'CUI', name: 'CUI', width: 200, align: 'left', search: false, editable: true,
             editrules: { edithidden: false }, hidedlg: true
-        },        
-        { label: 'CUI', name: 'idcui', width: 80, align: 'left', search: false, editable: true,
+        },
+        {
+            label: 'CUI', name: 'idcui', width: 80, align: 'left', search: false, editable: true, hidden: true,
             edittype: "select",
             editoptions: {
                 dataUrl: '/CUIs',
@@ -35,30 +40,30 @@ $(document).ready(function () {
                     var grid = $("#grid");
                     var rowKey = grid.getGridParam("selrow");
                     var rowData = grid.getRowData(rowKey);
-                    var thissid = rowData.divisionsponsor;
+                    var thissid = rowData.CUI;
                     console.log(response);
                     var data = JSON.parse(response);
                     var s = "<select>";//el default
                     s += '<option value="0">--Escoger CUI--</option>';
                     $.each(data, function (i, item) {
                         if (data[i].CUI == thissid) {
-                            s += '<option value="' + data[i].CUI + '" selected>' + data[i].nombre + '</option>';
+                            s += '<option value="' + data[i].cui + '" selected>' + data[i].nombre + '</option>';
                         } else {
-                            s += '<option value="' + data[i].CUI + '">' + data[i].nombre + '</option>';
+                            s += '<option value="' + data[i].cui + '">' + data[i].nombre + '</option>';
                         }
                     });
                     return s + "</select>";
-                },
+                }/*
                 dataEvents: [{
                     type: 'change', fn: function (e) {
                         $("input#divisionsponsor").val($('option:selected', this).text());
                     }
-                }],
-            }, dataInit: function (elem) { $(elem).width(200); }    
+                }],*/
+            }, dataInit: function (elem) { $(elem).width(200); }
         },
         { label: 'Nombre CUI', name: 'nombre', width: 250, align: 'left', search: false, editable: true },
-        { label: 'Responsable CUI', name: 'responsable', width: 250, align: 'left', search: false, editable: true},
-        { label: 'Ejercicio', name: 'ejercicio', width: 100, align: 'left', search: false, editable: true},
+        { label: 'Responsable CUI', name: 'responsable', width: 250, align: 'left', search: false, editable: true },
+        { label: 'Ejercicio', name: 'ejercicio', width: 100, align: 'left', search: false, editable: true },
         { label: 'Versión', name: 'version', width: 100, align: 'left', search: false, editable: true },
         { label: 'Nombre', name: 'descripcion', width: 150, align: 'left', search: false }
     ];
@@ -79,6 +84,7 @@ $(document).ready(function () {
         viewrecords: true,
         rowList: [5, 10, 20, 50],
         styleUI: "Bootstrap",
+        editurl: '/presupuesto/action',
         subGrid: true, // set the subGrid property to true to show expand buttons for each row
         subGridRowExpanded: showPresupuestoServicios, // javascript function that will take care of showing the child grid        
         loadError: function (jqXHR, textStatus, errorThrown) {
@@ -91,26 +97,26 @@ $(document).ready(function () {
     $("#grid").jqGrid('filterToolbar', { stringResult: true, searchOperators: true, searchOnEnter: false, defaultSearch: 'cn' });
 
     $('#grid').jqGrid('navGrid', "#pager", {
-        search: false, // show search button on the toolbar
         add: true,
         edit: false,
         del: false,
         refresh: true,
-        cloneToTop: false 
+        search: false, // show search button on the toolbar        
+        cloneToTop: false
     },
-    {
-        addCaption: "Agrega Presupuesto",
-        closeAfterAdd: true,
-        recreateForm: true,
-        //mtype: 'POST',
-        //url: '/iniciativas/add',
-        ajaxEditOptions: sipLibrary.jsonOptions,
-        serializeEditData: sipLibrary.createJSON,
-        template: tmpl,
-        errorTextFormat: function (data) {
-            return 'Error: ' + data.responseText
-        },
-        beforeSubmit: function (postdata, formid) {
+        {
+            addCaption: "Agrega Presupuesto0000",
+            closeAfterAdd: true,
+            recreateForm: true,
+            //mtype: 'POST',
+            //url: '/iniciativas/add',
+            ajaxEditOptions: sipLibrary.jsonOptions,
+            serializeEditData: sipLibrary.createJSON,
+            template: tmpl,
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
+            },
+            beforeSubmit: function (postdata, formid) {
                 if (postdata.CUI == 0) {
                     return [false, "División: Debe escoger un valor", ""];
                 } if (postdata.idejercicio == 0) {
@@ -122,6 +128,22 @@ $(document).ready(function () {
                 } else {
                     return [true, "", ""]
                 }
+            }
+        },
+        {
+            addCaption: "Modifica Presupuesto",
+            closeAfterEdit: true,
+            recreateForm: true,
+            ajaxEditOptions: sipLibrary.jsonOptions,
+            serializeEditData: sipLibrary.createJSON,
+            template: tmpl,
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
+            }
+        },
+        {
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
             }
         }
     );
@@ -143,7 +165,7 @@ $(document).ready(function () {
     $("#pager_left").css("width", "");
 });
 
-    
+
 function showPresupuestoServicios(parentRowID, parentRowKey) {
     var childGridID = parentRowID + "_table";
     var childGridPagerID = parentRowID + "_pager";
@@ -160,85 +182,92 @@ function showPresupuestoServicios(parentRowID, parentRowKey) {
         datatype: "json",
         page: 1,
         colModel: [
-                   { label: 'id',
-                      name: 'id',
-                      width: 50,
-                      key: true, 
-                      hidden:true
-                   },                                   
-                   { label: 'Cuenta',
-                     name: 'cuentacontable',
-                     search: false,
-                     width: 120
-                   },
-                   { label: 'Nombre Cuenta',
-                     name: 'nombrecuenta',
-                     width: 130,
-                     search: false,
-                     align: 'right'
-                   },
-                   { label: 'Servicio',
-                     name: 'nombre',  
-                     search: false,                 
-                     width: 200
-                   },                     
-                   { label: 'Moneda',
-                     name: 'moneda',
-                     width: 100,
-                     align: 'right',
-                     search: false
-                   },
-                   { label: 'Monto Forecast',
-                     name: 'montoforecast',
-                     width: 130,
-                     align: 'right',
-                     search: false,
-                     formatter: 'number', formatoptions: { decimalPlaces: 0 }
-                   },
-                   { label: 'Monto Anual',
-                     name: 'montoanual',
-                     width: 130,
-                     align: 'right',
-                     search: false,
-                     formatter: 'number', formatoptions: { decimalPlaces: 0 }
-                   }                                                     
+            {
+                label: 'id',
+                name: 'id',
+                width: 50,
+                key: true,
+                hidden: true
+            },
+            {
+                label: 'Cuenta',
+                name: 'cuentacontable',
+                search: false,
+                width: 120
+            },
+            {
+                label: 'Nombre Cuenta',
+                name: 'nombrecuenta',
+                width: 130,
+                search: false,
+                align: 'right'
+            },
+            {
+                label: 'Servicio',
+                name: 'nombre',
+                search: false,
+                width: 200
+            },
+            {
+                label: 'Moneda',
+                name: 'moneda',
+                width: 100,
+                align: 'right',
+                search: false
+            },
+            {
+                label: 'Monto Forecast',
+                name: 'montoforecast',
+                width: 130,
+                align: 'right',
+                search: false,
+                formatter: 'number', formatoptions: { decimalPlaces: 0 }
+            },
+            {
+                label: 'Monto Anual',
+                name: 'montoanual',
+                width: 130,
+                align: 'right',
+                search: false,
+                formatter: 'number', formatoptions: { decimalPlaces: 0 }
+            }
         ],
         viewrecords: true,
-        styleUI: "Bootstrap", 
+        styleUI: "Bootstrap",
         regional: 'es',
         sortable: "true",
         rowNum: 10,
- 		height: 'auto',  
+        height: 'auto',
         rowList: [5, 10, 20, 50],
-        autowidth:false,       
+        autowidth: false,
         subGrid: true, // set the subGrid property to true to show expand buttons for each row
         subGridRowExpanded: showProyectoErogaciones, // javascript function that will take care of showing the child grid                
         pager: "#" + childGridPagerID
     });
-    
+
     $("#" + childGridID).jqGrid('filterToolbar', { stringResult: true, searchOperators: true, searchOnEnter: false, defaultSearch: 'cn' });
 
-    $("#" + childGridID).jqGrid('navGrid', "#"+ childGridPagerID, {
+    $("#" + childGridID).jqGrid('navGrid', "#" + childGridPagerID, {
         search: false, // show search button on the toolbar
         add: false,
         edit: false,
         del: false,
         refresh: true
-    });    
-    
+    });
+
     $("#" + childGridID).jqGrid('navButtonAdd', "#" + childGridPagerID, {
-    caption: "Excel",
-    buttonicon: "silk-icon-page-excel",
-    title: "Excel",
-    position: "last",
-    onClickButton: function () {
-        var grid = $("#" + childGridID);
-        var rowKey = grid.getGridParam("selrow");
-        var url = '/presupuestoserviciosexcel/'+ parentRowKey;
-        $("#" + childGridID).jqGrid('excelExport', { "url": url });
-    }
-});
-    
+        caption: "Excel",
+        buttonicon: "silk-icon-page-excel",
+        title: "Excel",
+        position: "last",
+        onClickButton: function () {
+            var grid = $("#" + childGridID);
+            var rowKey = grid.getGridParam("selrow");
+            var url = '/presupuestoserviciosexcel/' + parentRowKey;
+            $("#" + childGridID).jqGrid('excelExport', { "url": url });
+        }
+    });
+
 }
 
 function showProyectoErogaciones(parentRowID, parentRowKey) {
@@ -247,7 +276,7 @@ function showProyectoErogaciones(parentRowID, parentRowKey) {
 
     // send the parent row primary key to the server so that we know which grid to show
     var childGridURL = "/erogacioneslist/" + parentRowKey;
-    
+
     // add a table and pager HTML elements to the parent grid row - we will render the child grid here
     $('#' + parentRowID).append('<table id=' + childGridID + '></table><div id=' + childGridPagerID + ' class=scroll></div>');
 
@@ -257,79 +286,86 @@ function showProyectoErogaciones(parentRowID, parentRowKey) {
         datatype: "json",
         page: 1,
         colModel: [
-                   { label: 'id',
-                      name: 'id',
-                      width: 50,
-                      key: true,
-                      hidden:true
-                   },                   
-                   { label: 'Nombre Proveedor',
-                     name: 'razonsocial',
-                     width: 250,
-                   },
-                   { label: 'Numero Factura',
-                     name: 'factura',
-                     align: 'center',
-                     width: 100,
-                   },
-                   { label: 'Fecha GL',
-                     name: 'fechagl',
-                     search: false,
-                     sortable: false,
-                     width: 100,
-                     formatter: 'date', formatoptions: { srcformat: 'ISO8601Long', newformat: 'Y-m-d' }
-                   },
-                   { label: 'Tarea Ajustada',
-                     name: 'numerotarea',
-                     align: 'center',
-                     search: false,
-                     width: 100,
-                   },      
-                   { label: 'Tarea Original',
-                     name: 'toriginalactual',
-                     align: 'center',
-                     search: false,
-                     width: 100,
-                   },  
-                   { label: 'Total',
-                     name: 'montosum',
-                     search: false,
-                     width: 150,
-                     align: 'right',
-                     formatter: 'number', formatoptions: { decimalPlaces: 0 }
-                   }                                              
+            {
+                label: 'id',
+                name: 'id',
+                width: 50,
+                key: true,
+                hidden: true
+            },
+            {
+                label: 'Nombre Proveedor',
+                name: 'razonsocial',
+                width: 250,
+            },
+            {
+                label: 'Numero Factura',
+                name: 'factura',
+                align: 'center',
+                width: 100,
+            },
+            {
+                label: 'Fecha GL',
+                name: 'fechagl',
+                search: false,
+                sortable: false,
+                width: 100,
+                formatter: 'date', formatoptions: { srcformat: 'ISO8601Long', newformat: 'Y-m-d' }
+            },
+            {
+                label: 'Tarea Ajustada',
+                name: 'numerotarea',
+                align: 'center',
+                search: false,
+                width: 100,
+            },
+            {
+                label: 'Tarea Original',
+                name: 'toriginalactual',
+                align: 'center',
+                search: false,
+                width: 100,
+            },
+            {
+                label: 'Total',
+                name: 'montosum',
+                search: false,
+                width: 150,
+                align: 'right',
+                formatter: 'number', formatoptions: { decimalPlaces: 0 }
+            }
         ],
         viewrecords: true,
         rowNum: 10,
- 		height: 'auto',
-        styleUI: "Bootstrap",         
-        autowidth:false, 
-        sortable: "true",  
-        rowList: [5, 10, 20, 50],    
-        regional : "es",
+        height: 'auto',
+        styleUI: "Bootstrap",
+        autowidth: false,
+        sortable: "true",
+        rowList: [5, 10, 20, 50],
+        regional: "es",
         pager: "#" + childGridPagerID
     });
 
     $("#" + childGridID).jqGrid('filterToolbar', { stringResult: true, searchOperators: true, searchOnEnter: false, defaultSearch: 'cn' });
 
-    $("#" + childGridID).jqGrid('navGrid', "#"+ childGridPagerID, {
+    $("#" + childGridID).jqGrid('navGrid', "#" + childGridPagerID, {
         search: false, // show search button on the toolbar
         add: false,
         edit: false,
         del: false,
         refresh: true
-    });  
+    });
 
     $("#" + childGridID).jqGrid('navButtonAdd', "#" + childGridPagerID, {
-    caption: "Excel",
-    buttonicon: "silk-icon-page-excel",
-    title: "Excel",
-    position: "last",
-    onClickButton: function () {
-        var grid = $("#" + childGridID);
-        var rowKey = grid.getGridParam("selrow");
-        var url = '/erogacionesexcel/'+ parentRowKey;
-        $("#" + childGridID).jqGrid('excelExport', { "url": url });
-    }
+        caption: "Excel",
+        buttonicon: "silk-icon-page-excel",
+        title: "Excel",
+        position: "last",
+        onClickButton: function () {
+            var grid = $("#" + childGridID);
+            var rowKey = grid.getGridParam("selrow");
+            var url = '/erogacionesexcel/' + parentRowKey;
+            $("#" + childGridID).jqGrid('excelExport', { "url": url });
+        }
     });
 }
