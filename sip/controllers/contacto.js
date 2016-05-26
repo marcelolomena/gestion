7000abe187849d43e8501b84a170260f8edc7116
rodrigocaceres <rodrigocaceres@starmedia.com>
2016-05-26 16:33:31 -0400

    Mantenedor de Proveedores

--- sip/controllers/contacto.js
+++ sip/controllers/contacto.js
@@ -1,7 +1,7 @@
 var models = require('../models');
 var sequelize = require('../models/index').sequelize;
 var nodeExcel = require('excel-export');
-
+var utilSeq = require('../utils/seq');
 
 exports.getContactos = function (req, res) {
   //var idContrato = req.params.id
@@ -18,7 +18,7 @@ exports.getContactos = function (req, res) {
 };
 
 // Create endpoint /proyecto for GET
-exports.getContacto = function (req, res) {
+exports.getContactos = function (req, res) {
   // Use the Proyectos model to find all proyectos
   var page = req.query.page;
   var rows = req.query.rows;
@@ -43,7 +43,7 @@ exports.getContacto = function (req, res) {
     "set @pageNum=" + page + ";   " +
     "With SQLPaging As   ( " +
     "Select Top(@rowsPerPage * @pageNum) ROW_NUMBER() OVER (ORDER BY " + order + ") " +
-    "as resultNum, contacto,fono,correo " +
+    "as resultNum,id, contacto,fono,correo " +
     "FROM sip.contactoproveedor where idproveedor=" + id + ")" +
     "select * from SQLPaging with (nolock) where resultNum > ((@pageNum - 1) * @rowsPerPage);";
 
@@ -64,7 +64,7 @@ exports.getContacto = function (req, res) {
         "set @pageNum=" + page + ";   " +
         "With SQLPaging As   ( " +
         "Select Top(@rowsPerPage * @pageNum) ROW_NUMBER() OVER (ORDER BY " + order + ") " +
-        "as resultNum, contacto,fono,correo " +
+        "as resultNum, id,contacto,fono,correo " +
         "FROM sip.contactoproveedor WHERE " + condition.substring(0, condition.length - 4) + ")" +
         "select * from SQLPaging with (nolock) where resultNum > ((@pageNum - 1) * @rowsPerPage);";
 
@@ -101,3 +101,101 @@ exports.getContacto = function (req, res) {
 
   }
 };
+
+exports.action = function (req, res) {
+  var action = req.body.oper;
+
+  switch (action) {
+    case "add":
+       models.ContactoProveedor.create({
+       contacto: req.body.contacto,
+       fono: req.body.fono,
+       correo: req.body.correo,
+       borrado    : 1
+      }).then(function (contactos) {
+        res.json({ error_code: 0 });
+      }).catch(function (err) {
+        console.log(err);
+        res.json({ error_code: 1 });
+      });
+      break;
+    case "edit":
+      models.ContactoProveedor.update({
+              contacto: req.body.contacto,
+              fono: req.body.fono,
+              correo: req.body.correo,
+              borrado: 1
+              }, {
+      where: {
+        id: req.body.id
+      }
+         }).then(function (contactos) {
+           res.json({ error_code: 0 });
+         }).catch(function (err) {
+           console.log(err);
+           res.json({ error_code: 1 });
+         });
+      break;
+    case "del":
+       models.ContactoProveedor.destroy({
+       where: {
+          id: req.body.id
+           }
+       }).then(function (rowDeleted) { // rowDeleted will return number of rows deleted
+       if (rowDeleted === 1) {
+          console.log('Deleted successfully');
+         }
+          res.json({ error_code: 0 });
+          }).catch(function (err) {
+          console.log(err);
+          res.json({ error_code: 1 });
+       });
+      break;
+  }
+
+};
+exports.list = function (req, res) {
+  var page = req.body.page;
+  var rows = req.body.rows;
+  var filters = req.body.filters;
+  var sidx = req.body.sidx;
+  var sord = req.body.sord;
+
+  if (!sidx)
+    sidx = "contacto";
+
+  if (!sord)
+    sord = "asc";
+
+  var orden = sidx + " " + sord;
+
+  var additional = [{
+    "field": "idproveedor",
+    "op": "eq",
+    "data": req.params.id
+  }];
+
+  utilSeq.buildAdditionalCondition(filters, additional, function (err, data) {
+    if (err) {
+      console.log("->>> " + err)
+    } else {
+      models.ContactoProveedor.count({
+        where: data
+      }).then(function (records) {
+        var total = Math.ceil(records / rows);
+        models.ContactoProveedor.findAll({
+          offset: parseInt(rows * (page - 1)),
+          limit: parseInt(rows),
+          order: orden,
+          where: data
+        }).then(function (contactos) {
+          res.json({ records: records, total: total, page: page, rows: contactos });
+        }).catch(function (err) {
+          //console.log(err);
+          res.json({ error_code: 1 });
+        });
+      })
+    }
+  });
+
+}
--- sip/public/javascripts/views/proveedor.js
+++ sip/public/javascripts/views/proveedor.js
@@ -2,22 +2,22 @@ $(document).ready(function () {
 
     var tmpl = "<div id='responsive-form' class='clearfix'>";
 
-        tmpl += "<div class='form-row'>";
-        tmpl += "<div class='column-full'>Rut {numrut}</div>";
-        tmpl += "</div>";
+    tmpl += "<div class='form-row'>";
+    tmpl += "<div class='column-full'>Rut {numrut}</div>";
+    tmpl += "</div>";
 
-        tmpl += "<div class='form-row'>";
-        tmpl += "<div class='column-full'>Razon Social {razonsocial}</div>";
-        tmpl += "</div>";
+    tmpl += "<div class='form-row'>";
+    tmpl += "<div class='column-full'>Razon Social {razonsocial}</div>";
+    tmpl += "</div>";
         
-        tmpl += "<div class='form-row'>";
-        tmpl += "<div class='column-full'>Negociador DIVOT {negociadordivot}</div>";
-        tmpl += "</div>";
+    tmpl += "<div class='form-row'>";
+    tmpl += "<div class='column-full'>Negociador DIVOT {negociadordivot}</div>";
+    tmpl += "</div>";
         
-        tmpl += "<hr style='width:100%;'/>";
-        tmpl += "<div> {sData} {cData}  </div>";
-        tmpl += "</div>";
-    
+    tmpl += "<hr style='width:100%;'/>";
+    tmpl += "<div> {sData} {cData}  </div>";
+    tmpl += "</div>";
+        
     var modelProveedor = [
         { label: 'id', name: 'id', key: true, hidden: true },
         { label: 'RUT', name: 'numrut', width: 150, align: 'center', search: false, editable: true, },
@@ -25,15 +25,26 @@ $(document).ready(function () {
         { label: 'Razón Social', name: 'razonsocial', width: 500, align: 'left', search: true, editable: true,formoptions: { rowpos: 1, colpos: 2 } },
         { label: 'Negociador DIVOT', name: 'negociadordivot', width: 300, align: 'left', search: true, editable: true,formoptions: { rowpos: 1, colpos: 2 } },
     ];
-        var modelContacto = [
-        { label: 'id', name: 'id', key: true, hidden: true },
-        { label: 'idproveedor', name: 'idproveedor', key: true, hidden: true },
-        { label: 'CONTACTO', name: 'contacto', width: 100, align: 'center', search: false, editable: true, },
-        { label: 'FONO', name: 'fono', width: 40, align: 'center', search: false, editable: true, },
-        { label: 'CORREO', name: 'correo', width: 600, align: 'left', search: true, editable: true, },
-    ];
+       
+    var tmpc = "<div id='responsive-form' class='clearfix'>";
+
+    tmpc += "<div class='form-row'>";
+    tmpc += "<div class='column-full'>Contacto {contacto}</div>";
+    tmpc += "</div>";
+
+    tmpc += "<div class='form-row'>";
+    tmpc += "<div class='column-full'>Telefono {fono}</div>";
+    tmpc += "</div>";
+        
+    tmpc += "<div class='form-row'>";
+    tmpc += "<div class='column-full'>Correo {correo}</div>";
+    tmpc += "</div>";
+        
+    tmpc += "<hr style='width:100%;'/>";
+    tmpc += "<div> {sData} {cData}  </div>";
+    tmpc += "</div>";         
     
-     $("#table_proveedor").jqGrid({
+    $("#table_proveedor").jqGrid({
         url: '/proveedores/list',
         mtype: "GET",
         datatype: "json",
@@ -42,13 +53,13 @@ $(document).ready(function () {
         rowNum: 10,
         regional: 'es',
         height: 'auto',
-        sortable: "true",
         width: null,
         shrinkToFit: false,
         caption: 'Lista de proveedores',
         pager: "#pager_proveedor",
         viewrecords: true,
         rowList: [5, 10, 20, 50],
+        editurl: '/proveedores/action',
         styleUI: "Bootstrap",
         subGrid: true, // set the subGrid property to true to show expand buttons for each row
         subGridRowExpanded: showContactos, // javascript function that will take care of showing the child grid        
@@ -58,21 +69,15 @@ $(document).ready(function () {
                 'errorThrown: ' + errorThrown);
         }
     });
-
     $("#table_proveedor").jqGrid('filterToolbar', { stringResult: true, searchOperators: true, searchOnEnter: false, defaultSearch: 'cn' });
 
-    $('#table_proveedor').jqGrid('navGrid', "#pager_proveedor", {
-        search: false, // show search button on the toolbar
-        add: true,
-        edit: true,
-        del: true,
-        refresh: true},
+    $('#table_proveedor').jqGrid('navGrid', "#pager_proveedor", {edit: true,add: true, del: true, search: false, refresh: true,view: false, position: "left", cloneToTop: false},                      
         {
             editCaption: "Modifica Proveedor",
             closeAfterEdit: true,
             recreateForm: true,
-            mtype: 'POST',
-            url: '/proveedores/update',
+          //  mtype: 'POST',
+          //  url: '/proveedores/update',
             ajaxEditOptions: sipLibrary.jsonOptions,
             serializeEditData: sipLibrary.createJSON,
             template: tmpl,
@@ -86,14 +91,65 @@ $(document).ready(function () {
                 else
                     return [true, "", ""]
             }, beforeShowForm: function (form) {
+                $('input#numrut', form).attr('readonly', 'readonly');
                 sipLibrary.centerDialog($('#table_proveedor').attr('id'));
+            }
+        },
+        {
+            addCaption: "Agrega Proveedor",
+            closeAfterAdd: true,
+            recreateForm: true,
+           // mtype: 'POST',
+           // url: '/proveedores/add',
+            ajaxEditOptions: sipLibrary.jsonOptions,
+            serializeEditData: sipLibrary.createJSON,
+            template: tmpl,
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
+            }, beforeSubmit: function (postdata, formid) {
+                if (postdata.numrut == 0) {
+                    return [false, "Rut: Debe escoger un valor", ""];
+                } if (postdata.razonsocial == 0) {
+                    return [false, "Razon Social: Debe escoger un valor", ""];
+                } if (postdata.negociadordivot == 0) {
+                    return [false, "Negociador DIVOT: Debe escoger un valor", ""];
+                } else {
+                    return [true, "", ""]
+                }
+            }, afterSubmit: function (response, postdata) {
+                var json = response.responseText;
+                var result = JSON.parse(json);
+                if (result.error_code != 0) {
+                    return [false, result.error_text, ""];
+                } else {
+                    var filters = "{\"groupOp\":\"AND\",\"rules\":[{\"field\":\"razonsocial\",\"op\":\"cn\",\"data\":\"" + postdata.razonsocial + "\"}]}";
+                    $("#table_proveedor").jqGrid('setGridParam', { search: true, postData: { filters } }).trigger("reloadGrid");
+                    return [true, "", ""];
+                }
+            }, beforeShowForm: function (form) {
+                sipLibrary.centerDialog($('#table_proveedor').attr('id'));
+            }, afterShowForm: function (form) {
+                sipLibrary.centerDialog($("#table_proveedor").attr('id'));
+            }
+        },
+        {
+            ajaxEditOptions: sipLibrary.jsonOptions,
+            serializeEditData: sipLibrary.createJSON,
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
+            }, afterSubmit: function (response, postdata) {
+                var json = response.responseText;
+                var result = JSON.parse(json);
+                if (result.error_code != 0)
+                    return [false, result.error_text, ""];
+                else
+                    return [true, "", ""]
             }
         },
         {
             recreateFilter: true
         }
     );
-
 
     $('#table_proveedor').jqGrid('navButtonAdd', '#pager_proveedor', {
         caption: "Excel",
@@ -107,99 +163,130 @@ $(document).ready(function () {
             $('#table_proveedor').jqGrid('excelExport', { "url": url });
         }
     });
- 
-    $("#pager_proveedor_left").css("width", "");
-});
-
+  
+    function showContactos(parentRowID, parentRowKey) {
+        var childGridID = parentRowID + "_table";
+        var childGridPagerID = parentRowID + "_pager";
+        var childGridURL = "/contactos/list/" + parentRowKey;    
     
-function showContactos(parentRowID, parentRowKey) {
-    var childGridID = parentRowID + "_table";
-    var childGridPagerID = parentRowID + "_pager";
+        var modelContacto = [          
+            { label: 'Contacto', name: 'contacto', width: 300, align: 'center', search: true, editable: true, },
+            { label: 'Telefono', name: 'fono', width: 100, align: 'center', search: false, editable: true, },
+            { label: 'Correo', name: 'correo', width: 300, align: 'left', search: false, editable: true, }
+        ];  
+        
+      $('#' + parentRowID).append('<table id=' + childGridID + '></table><div id=' + childGridPagerID + ' class=scroll></div>');
 
-    // send the parent row primary key to the server so that we know which grid to show
-    var childGridURL = "/contactos/list/" + parentRowKey;
 
-    // add a table and pager HTML elements to the parent grid row - we will render the child grid here
-    $('#' + parentRowID).append('<table id=' + childGridID + '></table><div id=' + childGridPagerID + ' class=scroll></div>');
-
-    $("#" + childGridID).jqGrid({
+      $("#" + childGridID).jqGrid({
         url: childGridURL,
         mtype: "GET",
+        cache: false,
         datatype: "json",
         page: 1,
-        colModel: [
-                   { label: 'id',
-                      name: 'id',
-                      width: 10,
-                      key: true, 
-                      hidden:true
-                   },
-                   { label: 'idproveedor',
-                      name: 'idproveedor',
-                      width: 10,
-                      key: false, 
-                      hidden:true
-                   },
-                   { label: 'Contacto',
-                     name: 'contacto',  
-                     search: false,                 
-                     width: 300
-                   },                                      
-                   { label: 'Telefono',
-                     name: 'fono',
-                     search: false,
-                     width: 120
-                   },
-                   { label: 'Correo',
-                     name: 'correo',
-                     width: 300,
-                     search: false,
-                   }
-                   
-        ],
+        colModel: modelContacto,
         viewrecords: true,
-        styleUI: "Bootstrap", 
+        styleUI: "Bootstrap",
         regional: 'es',
-        sortable: "true",
-        width: 740,
-        rowNum: 10,
- 		height: 'auto',  
-        rowList: [5, 10, 20, 50],
-        autowidth:false,       
-        subGrid: false, // set the subGrid property to true to show expand buttons for each row
-   //     subGridRowExpanded: showProyectoErogaciones, // javascript function that will take care of showing the child grid                
+        height: 'auto',
         pager: "#" + childGridPagerID,
+        editurl: '/contactos/action',
         gridComplete: function () {
                 var recs = $("#" + childGridID).getGridParam("reccount");
                 if (isNaN(recs) || recs == 0) {
 
-                    $("#" + childGridID).addRowData("blankRow", { "Contacto": "", "contacto": "No hay datos" });
+                    $("#" + childGridID).addRowData("blankRow", { "contacto": "", "fono": "No hay datos" });
                 }
             }
-    });
+      });
     
-    $("#" + childGridID).jqGrid('filterToolbar', { stringResult: true, searchOperators: true, searchOnEnter: false, defaultSearch: 'cn' });
-
-    $("#" + childGridID).jqGrid('navGrid', "#"+ childGridPagerID, {
-        search: false, // show search button on the toolbar
-        add: true,
-        edit: true,
-        del: true,
-        refresh: true
-    });    
+      $("#" + childGridID).jqGrid('navGrid', "#"+ childGridPagerID, {
+          edit: true, add: true,del: true,  search: false,  refresh: true, view: false, position: "left", cloneToTop: false
+      },
+        {
+            closeAfterEdit: true,
+            recreateForm: true,
+            ajaxEditOptions: sipLibrary.jsonOptions,
+            serializeEditData: sipLibrary.createJSON,
+            editCaption: "Modifica Contacto",
+            template: tmpc,
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
+            }, afterSubmit: function (response, postdata) {
+                var json = response.responseText;
+                var result = JSON.parse(json);
+                if (result.error_code != 0)
+                    return [false, result.error_text, ""];
+                else
+                    return [true, "", ""]
+            }, beforeShowForm: function (form) {
+                sipLibrary.centerDialog($("#" + childGridID).attr('id'));
+            }, afterShowForm: function (form) {
+                sipLibrary.centerDialog($("#" + childGridID).attr('id'));
+            }
+        },
+        {
+            closeAfterAdd: true,
+            recreateForm: true,
+            ajaxEditOptions: sipLibrary.jsonOptions,
+            serializeEditData: sipLibrary.createJSON,
+            addCaption: "Agrega contacto",
+            template: tmpc,
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
+            }, 
+            beforeSubmit: function (postdata, formid) {
+                if (postdata.contacto == 0) {
+                    return [false, "Contacto: Debe escoger un valor", ""];
+                } if (postdata.fono == 0) {
+                    return [false, "Telefono: Debe escoger un valor", ""];
+                } if (postdata.correo == 0) {
+                    return [false, "Correo: Debe escoger un valor", ""];
+                } else {
+                    return [true, "", ""]
+                }
+            }, 
+            afterSubmit: function (response, postdata) {
+                var json = response.responseText;
+                var result = JSON.parse(json);
+                if (result.error_code != 0) {
+                    return [false, result.error_text, ""];
+                } else {
+                    var filters = "{\"groupOp\":\"AND\",\"rules\":[{\"field\":\"contacto\",\"op\":\"cn\",\"data\":\"" + postdata.contacto + "\"}]}";
+                    $("#" + childGridID).jqGrid('setGridParam', { search: true, postData: { filters } }).trigger("reloadGrid");
+                    return [true, "", ""];
+                }
+            }, 
+            beforeShowForm: function (form) {
+                sipLibrary.centerDialog($("#" + childGridID).attr('id'));
+            }, 
+            afterShowForm: function (form) {
+                sipLibrary.centerDialog($("#" + childGridID).attr('id'));
+            }
+        },
+        {
+            closeAfterDelete: true,
+            recreateForm: true,
+            ajaxEditOptions: sipLibrary.jsonOptions,
+            serializeEditData: sipLibrary.createJSON,
+            addCaption: "Elimina Contacto",
+            errorTextFormat: function (data) {
+                return 'Error: ' + data.responseText
+            }, afterSubmit: function (response, postdata) {
+                var json = response.responseText;
+                var result = JSON.parse(json);
+                if (result.error_code != 0)
+                    return [false, result.error_text, ""];
+                else
+                    return [true, "", ""]
+            }
+        },
+        {
+            recreateFilter: true
+        }
+      ); 
     
-    $("#" + childGridID).jqGrid('navButtonAdd', "#" + childGridPagerID, {
-    caption: "Excel",
-    buttonicon: "silk-icon-page-excel",
-    title: "Excel",
-    position: "last",
-    onClickButton: function () {
-        var grid = $("#" + childGridID);
-        var rowKey = grid.getGridParam("selrow");
-        var url = '/contactosexcel/'+ parentRowKey;
-        $("#" + childGridID).jqGrid('excelExport', { "url": url });
-        }      
-    });
-
-    $("#" + childGridPagerID +"_left").css("width", "");
-}
+    }
+     
+    $("#pager_proveedor_left").css("width", "");
+});
