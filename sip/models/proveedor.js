29475a357ee565958734966863769de6d4c2ac12
Marcelo Lomeña <marcelol@loso.cl>
2016-05-30 11:42:33 -0300

    cambio modelo

--- sip/models/proveedor.js
+++ sip/models/proveedor.js
@@ -20,6 +20,10 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: true
     },
+    uid: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
     negociadordivot: {
       type: DataTypes.STRING,
       allowNull: true
