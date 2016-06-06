01f130e809bddf4f866f995572c426b00ac57350
Marcelo Lomeña <marcelol@loso.cl>
2016-06-06 18:32:00 -0400

    menu

--- sip/controllers/test.js
+++ sip/controllers/test.js
@@ -7,26 +7,113 @@ var log = function (inst) {
 
 exports.test = function (req, res) {
 
-    models.DetalleServicioCto.belongsTo(models.Contrato, { foreignKey: 'idcontrato' });
-    models.DetalleServicioCto.belongsTo(models.EstructuraCui, { foreignKey: 'idcui' });
-    models.DetalleServicioCto.belongsTo(models.Moneda, { foreignKey: 'idmoneda' });
-    models.DetalleServicioCto.belongsTo(models.Servicio, { foreignKey: 'idservicio' });
-    models.DetalleServicioCto.belongsTo(models.CuentasContables, { foreignKey: 'idcuenta', through: models.Servicio });
+    var Menu = function (id, desc, callback) {
+        var opt = [];
+        var item = {}
+        item["menu"] = desc;
+        opt.push(item);
 
-    models.DetalleServicioCto.findAll({
-        //attributes: ['id', 'user.nickname'],
-        where: { id: 415 },
-        include: [{ model: models.Contrato },
-            { model: models.EstructuraCui },
-            { model: models.Servicio },
+        try {
+            return models.Menu.findAll({
+                where: { 'pid': id }
+            }).then(function (submenu) {
+                var sub = []
+                submenu.forEach(function (opcion) {
+                    //console.log("opcion-------------> " + opcion.descripcion)
+                    sub.push({ [opcion.id]: opcion.descripcion })
+                });
+                opt.push(sub);
+                callback(opt)
+            }).catch(function (err) {
+                console.log("--------> " + err);
+            });
+        } catch (e) {
+            return callback(e);
+        }
+    }
+
+    var subMenu = function (op, menu, callback) {
+        return models.Menu.findAll({
+            where: { 'pid': menu.id }
+        }).then(function (submenu) {
+            var sub = []
+            var subsub = []
+            sub.push(op)
+            submenu.forEach(function (opcion) {
+                //console.log("opcion-------------> " + opcion.descripcion)
+                subsub.push({ [opcion.id]: opcion.descripcion })
+            });
+            sub.push(subsub)
+            //opt.push(sub);
+            callback(sub)
+        }).catch(function (err) {
+            console.log("--------> " + err);
+        });
+    }
+
+    var SuperMenu = function (user, callback) {
+        var opt = [];
+        var tot = [];
+        try {
+            var i = 0
+            var nombre = {}
+            nombre["nombre"] = user.first_name + " " + user.last_name
+            tot[0] = nombre
+            //console.dir(tot)
+            user.Rols.forEach(function (rol) {
+                //console.log("Rol-------------> " + rol.glosarol)
+
+                rol.Menus.forEach(function (menu) {
+                    //console.log("menu-------------> " + menu.id + " , " + menu.descripcion)
+                    var item = {}
+                    item["id"] = menu.id
+                    item["menu"] = menu.descripcion;
+
+                    subMenu(item, menu, function (submenu) {
+                        //console.dir(submenu)
+                        //opt.push( submenu );
+                        callback(submenu)
+                    });
+
+                });
+            });
+
+
+        } catch (e) {
+            return callback(e);
+        }
+    }
+
+    models.User.belongsToMany(models.Rol, { foreignKey: 'uid', through: models.UsrRol });
+    models.Rol.belongsToMany(models.User, { foreignKey: 'rid', through: models.UsrRol });
+    models.Rol.belongsToMany(models.Menu, { foreignKey: 'rid', through: models.RolFunc });
+    models.Menu.belongsToMany(models.Rol, { foreignKey: 'mid', through: models.RolFunc });
+    models.User.find({
+        where: { 'uid': 1 },
+        include: [
             {
-                model: models.CuentasContables
+                model: models.Rol,
+                include: [{ model: models.Menu, where: { 'pid': { $eq: null } }, required: false }]
             }
         ]
-    }).then(function (contratos) {
-        contratos.forEach(log)
+    }).then(function (user) {
+        console.log("usuario-------------> " + user.uname)
+        SuperMenu(user, function (submenu) {
+            console.dir(submenu)
+        });
+        /*
+                user.Rols.forEach(function (rol) {
+                    //console.log("Rol-------------> " + rol.glosarol)
+                    rol.Menus.forEach(function (menu) {
+                        //console.log("menu-------------> " + menu.id + " , " + menu.descripcion)
+                        Menu(menu.id, menu.descripcion, function (submenu) {
+                            console.dir(submenu)
+                        });
+                    });
+                });
+        */
     }).catch(function (err) {
-        console.log(err);
+        console.log("--------> " + err);
     });
 
 }
--- sip/models/rol_func.js
+++ sip/models/rol_func.js
@@ -1,7 +1,7 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('rol_func', {
+  return sequelize.define('RolFunc', {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
