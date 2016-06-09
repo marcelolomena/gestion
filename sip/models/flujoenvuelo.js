0d2ed9f8ce7c17e9cb8b213d1dcbb2c736669d83
Marcelo Lomeña <marcelol@loso.cl>
2016-06-09 12:00:01 -0400

    modelos

---
+++ sip/models/flujoenvuelo.js
@@ -0,0 +1,41 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('flujoenvuelo', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    iddetalleenvuelo: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'detalleenvuelo',
+        key: 'id'
+      }
+    },
+    periodo: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    idmoneda: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    presupuestoorigen: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    presupuestopesos: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    borrado: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    tableName: 'flujoenvuelo'
+  });
+};
---
+++ sip/models/proyectosenvuelo.js
@@ -0,0 +1,69 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('proyectosenvuelo', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    sap: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    codigoart: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    idcui: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    cui: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    porcentajeavance: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    fechainicio: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    fechapap: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    fechacierresap: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    uidlider: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    liderproyecto: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    uidpmo: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    pmoresponsable: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    borrado: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    tableName: 'proyectosenvuelo'
+  });
+};
