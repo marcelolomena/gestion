3aab9c399ec0ebb21d7b0e2f7a6c7632f6b964a0
Marcelo Lomeña <marcelol@loso.cl>
2016-08-11 15:31:54 -0400

    menu

--- sip/passport/init.js
+++ sip/passport/init.js
@@ -1,6 +1,7 @@
 var login = require('./login');
 var models = require('../models');
 var sequelize = require('../models/index').sequelize;
+var co = require('co');
 
 module.exports = function(passport) {
 
@@ -12,8 +13,6 @@ module.exports = function(passport) {
     passport.deserializeUser(function(id, done) {
 
         var subMenu = function(op, menu, callback) {
-            /*agregar query completa*/
-
             return models.menu.findAll({
                 where: { 'pid': menu.id }
             }).then(function(submenu) {
@@ -27,11 +26,42 @@ module.exports = function(passport) {
                 callback(undefined, opt)
             }).catch(function(err) {
                 callback(err, undefined)
-                //console.log("--------> " + err);
             });
         }
 
+        var NeoSubMenu = function(op, user, menu, callback) {
+            var sql_submenu = `
+                        select distinct e.id,e.descripcion,e.url from art_user a
+                        join sip.usr_rol b on a.uid = b.uid
+                        join sip.rol c on b.rid = c.id
+                        join sip.rol_func d on d.rid = c.id
+                        join sip.menu e on e.id = d.mid
+                        where a.uid=:uid and pid=:pid
+                      `
+            sequelize.query(sql_submenu,
+                {
+                    replacements: { uid: user.uid, pid: menu.id },
+                    type: sequelize.QueryTypes.SELECT
+                }
+            ).then(function(submenu) {
+                var opt = {}
+                opt["menu"] = op
+                var subsub = []
+
+                submenu.forEach(function(subopcion) {
+                    subsub.push({ "opt": subopcion.descripcion, "url": subopcion.url })
+                });
+                opt["submenu"] = subsub
+                callback(undefined, opt)
+
+            }).catch(function(err) {
+                callback(err, undefined)
+            });
+
+        }
+
         var NeoMenu = function(user, callback) {
+            var promises = []
             var sql_menu = `
                         select distinct e.id,e.descripcion from art_user a
                         join sip.usr_rol b on a.uid = b.uid
@@ -40,59 +70,54 @@ module.exports = function(passport) {
                         join sip.menu e on e.id = d.mid
                         where a.uid=:uid and pid is null
                       `
-            var sql_submenu = `
-                        select distinct e.id,e.descripcion from art_user a
-                        join sip.usr_rol b on a.uid = b.uid
-                        join sip.rol c on b.rid = c.id
-                        join sip.rol_func d on d.rid = c.id
-                        join sip.menu e on e.id = d.mid
-                        where a.uid=:uid and pid=:pid
-                      `
-            sequelize.query(sql_menu,
-                {
-                    replacements: { uid: user.uid },
-                    type: sequelize.QueryTypes.SELECT
-                }
-            ).then(function(rows) {
+            co(function* () {
+
+                var menu = yield sequelize.query(sql_menu,
+                    {
+                        replacements: { uid: user.uid },
+                        type: sequelize.QueryTypes.SELECT
+                    }
+                ).catch(function(err) {
+                    console.log(err)
+                    callback(err, 'undefined');
+                });
                 var todo = []
-                rows.forEach(function(menu) {
+                menu.forEach(function(menu) {
                     var item = {}
                     item["id"] = menu.id
                     item["menu"] = menu.descripcion
-                    //console.log(menu.id)
 
-                    sequelize.query(sql_submenu,
-                        {
-                            replacements: { uid: user.uid, pid: menu.id },
-                            type: sequelize.QueryTypes.SELECT
-                        }
-                    ).then(function(submenu) {
-                        var opt = {}
-                        opt["menu"] = item
-                        var subsub = []
-                        submenu.forEach(function(opcion) {
-                            subsub.push({ "opt": opcion.descripcion, "url": opcion.url })
+                    var promise = new Promise(function(resolve, reject) {
+                        NeoSubMenu(item, user, menu, function(err, submenu) {
+                            if (submenu)
+                                resolve(submenu);
+                            else
+                                reject(err)
                         });
-                        opt["submenu"] = subsub
-                        todo.push(opt);
-                    }).catch(function(err) {
-                        callback(err, 'undefined');
                     });
-
+                    promises.push(promise);
                 });
-                callback('undefined', todo);
+
+                return Promise.all(promises).then(function(items) {
+                    var menuPromises = [];
+                    for (var i = 0; i < items.length; i++) {
+                        menuPromises.push(items[i]);
+                    }
+
+                    callback(menuPromises);
+                });
+
             }).catch(function(err) {
                 callback(err, 'undefined');
+                console.log(err);
             });
+
         }
 
         var Menu = function(user, callback) {
             try {
                 var promises = []
-                //console.log("--->>>>>>> " + user.Rols.length);
-                //console.dir(user.Rols[0])
                 var rol = user.rols[0];
-                //user.Rols[0].forEach(function (rol) {
 
                 rol.menus.forEach(function(menu) {
                     var item = {}
@@ -109,14 +134,12 @@ module.exports = function(passport) {
                     });
                     promises.push(promise);
                 });
-                //});
 
                 return Promise.all(promises).then(function(items) {
                     var menuPromises = [];
                     for (var i = 0; i < items.length; i++) {
                         menuPromises.push(items[i]);
                     }
-                    //return Promise.all(menuPromises);
                     callback(menuPromises);
                 });
             } catch (e) {
@@ -137,7 +160,6 @@ module.exports = function(passport) {
                     include: [{ model: models.menu, where: { 'pid': { $eq: null } }, required: false }]
                 }
             ]
-            //group: ['[User].[first_name]', '[User].[last_name]', '[Rols.Menus].[descripcion]' , '[Rols.Menus].[url]' ]
         }).then(function(usr) {
             var usuario = []
             var nombre = {}
@@ -145,20 +167,28 @@ module.exports = function(passport) {
             nombre["uid"] = usr.uid
             nombre["rid"] = usr.rols[0].id
             usuario.push(nombre)
-
-
-            NeoMenu(usr, function(algo) {
-                console.dir(algo)
-            });            
-
-            Menu(usr, function(menu) {
+            NeoMenu(usr, function(menu) {
+                var supermenu = [];
+                menu.forEach(function(opt) {
+                    if (opt.submenu.length != 0) {
+                        supermenu.push(opt)
+                    }
+                });
                 var menus = {}
-                menus["menus"] = menu
+                menus["menus"] = supermenu
                 var user = usuario.concat(menus);
-                //console.dir(user[1].menus)
                 done(null, user);
             });
 
+            /*
+                        Menu(usr, function(menu) {
+                            var menus = {}
+                            menus["menus"] = menu
+                            var user = usuario.concat(menus);
+                            //console.dir(user[1].menus)
+                            done(null, user);
+                        });
+            */
 
 
         }).catch(function(err) {
@@ -168,7 +198,6 @@ module.exports = function(passport) {
 
     });
 
-    // Setting up Passport Strategies for Login and SignUp/Registration
     login(passport);
 
 }
