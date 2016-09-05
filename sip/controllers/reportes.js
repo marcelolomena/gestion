3e944f750d900cada2b51fabbef98dd5e613a551
marcelolomena <marcelol@loso.cl>
2016-09-05 00:08:23 -0300

    repo

--- sip/controllers/reportes.js
+++ sip/controllers/reportes.js
@@ -149,7 +149,7 @@ exports.troya = function (req, res) {
 exports.gerencias = function (req, res) {
 
     try {
-        var pathPdf = path.join(__dirname, '..', 'pdf')
+        var pathPdf = path.join(__dirname, '..', 'log')
 
         //console.log(pathPdf + path.sep)
 
@@ -157,55 +157,79 @@ exports.gerencias = function (req, res) {
 
         var helpers = fs.readFileSync(path.join(__dirname, '..', 'helpers', 'test.js'), 'utf8');
 
-        var sql =
-        `
-            select gerencia,periodo ,round(sum(caja),2) caja, round(sum(costo),2) costo from sip.v_reporte_presupuesto group by periodo,gerencia order by gerencia,periodo
+        var sql_1 =
+            `
+            select Y.nombre, sum(X.caja)/1000000 caja from sip.v_reporte_presupuesto X, sip.estructuracui Y 
+            where X.periodo >= 201509 AND X.periodo <= 201612 AND X.gerencia = Y.cui
+            group by Y.nombre
         `
 
-        sequelize.query(sql,
+        var sql_2 =
+            `
+            select Y.nombre, sum(X.caja)/1000000 caja from sip.v_reporte_presupuesto X, sip.estructuracui Y 
+            where X.periodo >= 201701 AND X.periodo <= 201712 AND X.gerencia = Y.cui
+            group by Y.nombre
+        `
+
+        sequelize.query(sql_1,
             {
                 //replacements: { perini:2016 },
                 type: sequelize.QueryTypes.SELECT
-            }).then(function (rows) {
+            }).then(function (rows_1) {
 
-                //console.dir(rows)
-                var datum = {
-                    "gerencias": rows
-                }
+                sequelize.query(sql_2,
+                    {
+                        //replacements: { perini:2016 },
+                        type: sequelize.QueryTypes.SELECT
+                    }).then(function (rows_2) {
 
-                jsreport.init().then(function () {
-                    return jsreport.render({
-                        template: {
-                            content: fs.readFileSync(path.join(__dirname, '..', 'templates', 'bar.html'), 'utf8'),
-                            helpers: helpers,
-                            engine: 'handlebars',
-                            recipe: 'phantom-pdf',
-                            phantom: {
-                                "orientation": "portrait",
-                                "format": "A4",
-                                "margin": "1cm",
-                                "headerHeight": "1cm"
-                            }
-                        },
-                        data: datum
-                    }).then(function (resp) {
-                        res.header('Content-disposition', 'inline; filename=' + filePdf);
-                        res.header('Content-type', 'application/pdf');
-                        resp.result.pipe(fs.createWriteStream(pathPdf + path.sep + filePdf))
-                            .on('finish', function () {
-                                fs.createReadStream(pathPdf + path.sep + filePdf).pipe(res)
+                        console.dir(rows_2)
+                        var datum = {
+                            "gerencias_1": rows_1,
+                            "gerencias_2": rows_2
+                        }
+
+                        jsreport.init().then(function () {
+                            return jsreport.render({
+                                template: {
+                                    content: fs.readFileSync(path.join(__dirname, '..', 'templates', 'bar.html'), 'utf8'),
+                                    helpers: helpers,
+                                    engine: 'handlebars',
+                                    recipe: 'phantom-pdf',
+                                    phantom: {
+                                        "orientation": "portrait",
+                                        "format": "A4",
+                                        "margin": "1cm",
+                                        "headerHeight": "1cm"
+                                    }
+                                },
+                                data: datum
+                            }).then(function (resp) {
+                                res.header('Content-disposition', 'inline; filename=' + filePdf);
+                                res.header('Content-type', 'application/pdf');
+                                resp.result.pipe(fs.createWriteStream(pathPdf + path.sep + filePdf))
                                     .on('finish', function () {
-                                        fs.unlink(pathPdf + path.sep + filePdf);
-                                        //console.log('finalizo');
+                                        fs.createReadStream(pathPdf + path.sep + filePdf).pipe(res)
+                                            .on('finish', function () {
+                                                fs.unlink(pathPdf + path.sep + filePdf);
+                                                //console.log('finalizo');
+                                            });
                                     });
-                            });
+
+                            }).catch(function (e) {
+                                console.log(e)
+                            })
+                        }).catch(function (e) {
+                            console.log(e)
+                        })
+
 
                     }).catch(function (e) {
                         console.log(e)
                     })
-                }).catch(function (e) {
-                    console.log(e)
-                })
+
+
+
 
             }).catch(function (err) {
                 console.log(err)
