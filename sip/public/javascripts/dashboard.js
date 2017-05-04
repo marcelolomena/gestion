501241f5770797b2fa5b642e1f0e1b6ce5d5ed9d
Marcelo Lomeña <marcelol@loso.cl>
2017-05-04 11:20:52 -0300

    new dynamic templates PUG

--- sip/public/javascripts/dashboard.js
+++ sip/public/javascripts/dashboard.js
@@ -1,7 +1,59 @@
 $(document).ready(function () {
-    Highcharts.setOptions({
-        global: {
-            useUTC: false
+
+    Highcharts.theme = {
+        colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+            '#FF9655', '#FFF263', '#6AF9C4'],
+        chart: {
+            backgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgb(255, 255, 255)'],
+                    [1, 'rgb(240, 240, 255)']
+                ]
+            },
+        },
+        title: {
+            style: {
+                color: '#000',
+                font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
+            }
+        },
+        subtitle: {
+            style: {
+                color: '#666666',
+                font: 'bold 12px "Trebuchet MS", Verdana, sans-serif'
+            }
+        },
+
+        legend: {
+            itemStyle: {
+                font: '9pt Trebuchet MS, Verdana, sans-serif',
+                color: 'black'
+            },
+            itemHoverStyle: {
+                color: 'gray'
+            }
+        }
+    };
+
+    Highcharts.setOptions(Highcharts.theme, {
+        lang: {
+            loading: 'Cargando...',
+            months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            weekdays: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'],
+            shortMonths: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+            exportButtonTitle: "Exportar",
+            printButtonTitle: "Importar",
+            rangeSelectorFrom: "De",
+            rangeSelectorTo: "A",
+            rangeSelectorZoom: "Periodo",
+            downloadPNG: 'Descargar gráfica PNG',
+            downloadJPEG: 'Descargar gráfica JPEG',
+            downloadPDF: 'Descargar gráfica PDF',
+            downloadSVG: 'Descargar gráfica SVG',
+            printChart: 'Imprimir Gráfica',
+            thousandsSep: ",",
+            decimalPoint: '.'
         }
     });
 
@@ -64,7 +116,7 @@ $(document).ready(function () {
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
+                text: 'MM'
             }
         },
         plotOptions: {
@@ -75,7 +127,10 @@ $(document).ready(function () {
                 enableMouseTracking: false
             }
         },
-        series: []
+        series: [],
+        exporting: {
+            enabled: true
+        }
     }
 
     var dona = {
@@ -157,7 +212,9 @@ $(document).ready(function () {
         plotOptions: {
             column: {
                 pointPadding: 0.2,
-                borderWidth: 0
+                borderWidth: 0,
+            }, series: {
+                colorByPoint: true
             }
         },
         series: []
