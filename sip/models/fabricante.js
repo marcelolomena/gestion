47dba0c1a344ebbc53b75f3c82f741167ee73185
Juan Pablo Schmiede <jschmiede@hotmail.com>
2017-09-11 16:54:09 -0300

    modelos 1

---
+++ sip/models/fabricante.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('fabricante', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre: {
+            type: DateTypes.STRING(120),
+            allowNull:false
+
+        },
+        borrado: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        }
+    }, {
+        schema: 'lic',
+        timestamps: false,
+        tableName: 'fabricante'
+    });
+};
