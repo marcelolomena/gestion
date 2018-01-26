75bb61dc4c4d641f1dc221ecbad974326cc68876
javcastrof <jcastro@zrismart.cl>
2018-01-26 16:26:26 -0300

    fix buscar producto traduccion

--- sip/public/javascripts/views/lic/traduccion.js
+++ sip/public/javascripts/views/lic/traduccion.js
@@ -4,73 +4,84 @@
     $(function () {
         var $table = $('#gridMaster');
         var viewModel = [{
-            label: 'ID',
-            name: 'id',
-            key: true,
-            hidden: true,
-            editable: false
-        },
-        {
-            label: 'Producto',
-            name: 'idProducto',
-            jsonmap: 'producto.nombre',
-            width: 250,
-            align: 'center',
-            sortable: false,
-            editable: true,
-            edittype: 'select',
-            editoptions: {
-                dataUrl: '/lic/producto',
-                buildSelect: function (response) {
-                    var rowData = $table.getRowData($table.getGridParam('selrow'));
-                    var thissid = rowData.nombre;
-                    var data = JSON.parse(response);
-                    return new zs.SelectTemplate(data, 'Seleccione el Producto', thissid).template;
-                },
-                dataEvents: [{
-                    type: 'change',
-                    fn: function (e) {
-                        var rowKey = $table.getGridParam("selrow");
-                        var rowData = $table.getRowData(rowKey);
-                        var thissid = $(this).val();
-                        $("input#idFabricante").val($('option:selected', this).text());
-                        var idFabricante = $('option:selected', this).val();
-                        if (idFabricante) {
+                label: 'ID',
+                name: 'id',
+                key: true,
+                hidden: true,
+                editable: false
+            },
+            {
+                label: 'Producto',
+                name: 'idProducto',
+                jsonmap: 'producto.nombre',
+                width: 250,
+                align: 'center',
+                sortable: false,
+                editable: true,
+                edittype: 'select',
+                editoptions: {
+                    dataUrl: '/lic/producto',
+                    buildSelect: function (response) {
+                        var rowData = $table.getRowData($table.getGridParam('selrow'));
+                        var thissid = rowData.nombre;
+                        var data = JSON.parse(response);
+                        return new zs.SelectTemplate(data, 'Seleccione el Producto', thissid).template;
+                    },
+                    dataEvents: [{
+                        type: 'change',
+                        fn: function (e) {
+                            var rowKey = $table.getGridParam("selrow");
+                            var rowData = $table.getRowData(rowKey);
+                            var thissid = $(this).val();
+                            $("input#idFabricante").val($('option:selected', this).text());
+                            var idFabricante = $('option:selected', this).val();
+                            if (idFabricante) {
 
-                            $.ajax({
-                                type: "GET",
-                                url: '/lic/getFabricante/' + thissid,
-                                async: false,
-                                success: function (data) {
-                                    if (data) {
-                                        $("select#idFabricante").val(data.idFabric);
-                                        $("#idFabricante").attr('disabled', true);
-                                        $("#otroFabricante").attr('disabled', true);
-                                    } else {
-                                        alert("No existe Fabricante para este Producto");
-                                        $("select#idFabricante").val("");
+                                $.ajax({
+                                    type: "GET",
+                                    url: '/lic/getFabricante/' + thissid,
+                                    async: false,
+                                    success: function (data) {
+                                        if (data) {
+                                            $("select#idFabricante").val(data.idFabric);
+                                            $("#idFabricante").attr('disabled', true);
+                                            $("#otroFabricante").attr('disabled', true);
+                                        } else {
+                                            alert("No existe Fabricante para este Producto");
+                                            $("select#idFabricante").val("");
+                                        }
                                     }
-                                }
 
-                            });
-                        } else {
-                            $("#idFabricante").attr('disabled', false);
-                            $("#otroFabricante").attr('disabled', false);
+                                });
+                            } else {
+                                $("#idFabricante").attr('disabled', false);
+                                $("#otroFabricante").attr('disabled', false);
 
+                            }
                         }
+                    }],
+                },
+                editrules: {
+                    required: false
+                },
+                search: true,
+                stype: 'select',
+                searchoptions: {
+                    dataUrl: '/lic/producto',
+                    buildSelect: function (response) {
+                        var rowData = $table.getRowData($table.getGridParam('selrow'));
+                        var thissid = rowData.idProducto;
+                        var data = JSON.parse(response);
+                        return new zs.SelectTemplate(data, 'Seleccione', thissid).template;
                     }
-                }],
-            },
-            editrules: {
-                required: false
-            },
-            search: true
-        }, {
-            label: 'Nombre',
-            name: 'nombre',
-            width: 300,
-            editable: true
-        }];
+                }
+            }, {
+                label: 'Nombre',
+                name: 'nombre',
+                width: 300,
+                editable: true
+            }
+        ];
         var grid = new zs.SimpleGrid('gridMaster', 'pagerMaster', 'Traducciones', 'Editar Traducción', 'Agregar Traducción', '/lic/traduccion', viewModel, 'idProducto', '/lic/getsession', ['Administrador LIC']);
         grid.build();
     });
