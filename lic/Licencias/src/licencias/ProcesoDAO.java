ec6e4b8eed142bfd34266a62b409110e08790732
ricreyes <ricreyesman@gmail.com>
2018-03-14 17:41:08 -0300

    otros java

--- lic/Licencias/src/licencias/ProcesoDAO.java
+++ lic/Licencias/src/licencias/ProcesoDAO.java
@@ -7,6 +7,8 @@ package licencias;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.HashMap;
 
 /**
  *
@@ -19,9 +21,10 @@ public class ProcesoDAO {
         Connection con = null;
         try {
             con = DBConecction.getInstance().getConnection();
-            cstmt = con.prepareCall("{ call lic.alertaRenoSoporteSIN }");
+            cstmt = con.prepareCall("{ call lic.alertaRenoSoporteSIN () }");
             System.out.println("Ejecutando:alertaRenoSoporteSIN");
-            cstmt.execute();
+            int cant= cstmt.executeUpdate();
+            System.out.println("cant:"+cant);
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
@@ -42,6 +45,49 @@ public class ProcesoDAO {
             }
         }
 
+    }
+
+    public HashMap obtieneBancos() throws Exception {
+        HashMap hm = new HashMap();
+        ResultSet rs = null;
+        CallableStatement cstmt	= null;
+        Connection con = null;
+        try {
+            con = DBConecction.getInstance().getConnection();
+            cstmt = con.prepareCall("{ call lic.sololist () }");
+            rs = cstmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("Listando:" + rs.getString("nombre"));
+                //hm.put(rs.getString("vcCodigo"), new Integer(rs.getInt("iIdBanco")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+	} finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (cstmt != null) {
+                    cstmt.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return hm;
     }
 
 
